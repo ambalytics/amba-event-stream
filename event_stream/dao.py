ac22d66c7cc5bb701099d1cadd8f3ca1719ebc84
Lukas Jesche <ljaschka@gmail.com>
2021-10-04 11:11:10 +0200

    - updates

--- event_stream/dao.py
+++ event_stream/dao.py
@@ -10,6 +10,25 @@ from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import sessionmaker, scoped_session
 
 
+def save_publication_not_found(session, doi):
+    obj = PublicationNotFound(
+        publication_doi=doi,
+        last_try=datetime.datetime.now(),
+    )
+
+    table = PublicationNotFound
+    kwargs = {'publication_doi': doi}
+    obj_db = DAO.get_object(session, table, kwargs)
+    if obj_db:
+        # add count to existing object
+        obj_db.last_try = datetime.datetime.now()
+        session.commit()
+        return obj_db
+
+    DAO.save_object(session, obj)
+    return obj
+
+
 def save_newest_discussion_subj(session, e):
     max_count = 10
 
